cda829976e616ace6dc77b6e35c20747f9317c22
daniela <daniela.lucia.villa@hotmail.com>
2024-03-31 21:00:47 -0300

    borrado db y agregada de nuevo

--- backend/Backend/Endpoints/AuthEndpoints.cs
+++ backend/Backend/Endpoints/AuthEndpoints.cs
@@ -2,12 +2,12 @@
 
 namespace Backend.Endpoints
 {
-    public class AuthEndpoints(IConfiguration configuration) : ICarterModule
-    {
-        //50:00
-        public void AddRoutes(IEndpointRouteBuilder app)
-        {
-            throw new NotImplementedException();
-        }
-    }
+    //public class AuthEndpoints(IConfiguration configuration) : ICarterModule
+    //{
+    //    //50:00
+    //    public void AddRoutes(IEndpointRouteBuilder app)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 }
